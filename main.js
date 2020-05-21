5b6365a68d143263646e149850c43f7725210fea
julian lago <julianlago93@gmail.com>
2020-05-21 18:49:08 -0300

    Union de formulario mensual y anual.

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-const $botonCalcularMensual = document.querySelector("#botonsueldomensual");
-$botonCalcularMensual.onclick = function(){
-
-
-    let sueldoAnual=document.getElementById("inputsueldoanual").value;
-    let sueldoMensual=sueldoAnual/12;
-    document.getElementById("showsueldomensual").value= sueldoMensual;
-    
-    return false;
-}
-
 const $botonCalcularAnual = document.querySelector("#botonsueldoanual");
 $botonCalcularAnual.onclick = function(){
 
@@ -20,13 +9,24 @@ $botonCalcularAnual.onclick = function(){
     return false;
 }
 
+const $botonCalcularMensual = document.querySelector("#botonsueldomensual");
+$botonCalcularMensual.onclick = function(){
+
+
+    let sueldoAnual=document.getElementById("inputsueldomensual").value;
+    let sueldoMensual=sueldoAnual/12;
+    document.getElementById("showsueldoanual").value= sueldoMensual;
+    
+    return false;
+}
+
 let $Botonuserdata=document.querySelector("#botonuserdata");
 $Botonuserdata.onclick = function () {
     let nombreUsuario = document.getElementById("nombreusuario").value;
     let nombreUsuario2 = document.getElementById("segundonombreusuario").value;
     let apellidoUsuario = document.getElementById("apellidousuario").value;
     let edadUsuario= String(document.getElementById("edadusuario").value);
-    let sueldoMensualUsuario=String(document.getElementById("showsueldomensual").value);
+    let sueldoMensualUsuario=String(document.getElementById("showsueldoanual").value);
     document.getElementById("header1").innerHTML="Bienvenid@ " + nombreUsuario + "!";
     
     let userNombre ="Nombres y Apellido: " +  nombreUsuario + " "+ nombreUsuario2 + " " + apellidoUsuario ;
@@ -44,10 +44,15 @@ $Botonuserdata.onclick = function () {
         document.getElementById("userdata").appendChild(showUserEdad);    
 
     let showUserSueldo = document.createElement("input");
-        showUserSueldo.value="Sueldo Mensual: "+ userSueldo;
+        showUserSueldo.value="Sueldo: "+ userSueldo;
         showUserSueldo.className="input-user-data";
         document.getElementById("userdata").appendChild(showUserSueldo);    
 
+
+    let $p  = document.createElement("input");    
+    $p.id="input-break";
+        document.getElementById("userdata").appendChild($p);
+    
    return false;
     
 
